a1120616ec5359487f2ebd238c48cb5920a216db
Siddharth Dobhal <dobhal.sidharth770@gmail.com>
2025-12-23 11:16:42 +0530

    sid

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.adfinityclarity.com',
+          },
+        ],
+        destination: 'https://adfinityclarity.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
