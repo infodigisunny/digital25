d6ab2f6bef1ba9eabb880019b972f90d941a8570
infodigisunny <infodigisunny@gmail.com>
2026-01-10 23:15:04 +0530

    s

--- src/app/HomeClient.js
+++ src/app/HomeClient.js
@@ -76,20 +76,7 @@ export default function HomeClient() {
         handleMobileMenu={handleMobileMenu} 
       />
 
-      {/* ===================== GTM (OPTIONAL IF NOT IN LAYOUT) ===================== */}
-      <Script
-        id="gtm-home"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NDZMJK42');
-          `,
-        }}
-      />
+      
 
       {/* ===================== HERO + FORM SECTION ===================== */}
       <section className="section-box">
