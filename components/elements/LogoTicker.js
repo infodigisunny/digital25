a3457c5603265ca2f5eac78eb5b5a03d63c4c159
infodigisunny <infodigisunny@gmail.com>
2026-02-18 21:18:10 +0530

    s

--- components/elements/LogoTicker.js
+++ components/elements/LogoTicker.js
@@ -9,35 +9,35 @@ export default function LogoTicker() {
                     pauseOnHover={true}
                     direction="left" className="carouselTicker__list list-logos">
                     <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo2.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo3.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo4.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo5.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo6.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo7.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo8.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo9.png" alt="Adfinity Clarity" /></div>
-                    </li>
-                    <li className="carouselTicker__item">
-                        <div className="item-logo"><img src="/assets/imgs/page/homepage3/logo1.png" alt="Adfinity Clarity" /></div>
-                    </li>
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/ahrefs.svg" alt="Ahrefs" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/backlinko.svg" alt="Backlinko" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googleanalytics.svg" alt="Google Analytics" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googlelookerstudio.svg" alt="Google Looker Studio" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googlesearchconsole.svg" alt="Google Search Console" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/hotjar.svg" alt="Hotjar" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/hubSpot.svg" alt="HubSpot" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/moz.svg" alt="Moz" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/searchenginejournal.svg" alt="Search Engine Journal" /></div>
+                </li>
+                <li className="carouselTicker__item">
+                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/ubersuggest.svg" alt="UberSuggest" /></div>
+                </li>
                 </Marquee>
         </>
     )
