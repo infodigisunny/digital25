1851389f2f44f1d1de104648a9e219dafb69be99
infodigisunny <infodigisunny@gmail.com>
2026-02-18 21:22:32 +0530

    s

--- components/elements/LogoTicker.js
+++ components/elements/LogoTicker.js
@@ -9,38 +9,92 @@ export default function LogoTicker() {
                     pauseOnHover={true}
                     direction="left" className="carouselTicker__list list-logos">
                     <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/ahrefs.svg" alt="Ahrefs" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/backlinko.svg" alt="Backlinko" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googleanalytics.svg" alt="Google Analytics" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googlelookerstudio.svg" alt="Google Looker Studio" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/googlesearchconsole.svg" alt="Google Search Console" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/hotjar.svg" alt="Hotjar" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/hubSpot.svg" alt="HubSpot" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/moz.svg" alt="Moz" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/searchenginejournal.svg" alt="Search Engine Journal" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/ubersuggest.svg" alt="UberSuggest" /></div>
-                </li>
-                <li className="carouselTicker__item">
-                    <div className="item-logo"><img src="/assets/imgs/page/homepage3/yoastseo.svg" alt="Yoast SEO" /></div>
-                </li>
+  <a href="https://ahrefs.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/ahrefs.svg" alt="Ahrefs" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://backlinko.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/backlinko.svg" alt="Backlinko" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://analytics.google.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/googleanalytics.svg" alt="Google Analytics" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://lookerstudio.google.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/googlelookerstudio.svg" alt="Google Looker Studio" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://search.google.com/search-console" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/googlesearchconsole.svg" alt="Google Search Console" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://www.hotjar.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/hotjar.svg" alt="Hotjar" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://hubspot.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/hubSpot.svg" alt="HubSpot" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://moz.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/moz.svg" alt="Moz" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://www.searchenginejournal.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/searchenginejournal.svg" alt="Search Engine Journal" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://neilpatel.com/ubersuggest/" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/ubersuggest.svg" alt="UberSuggest" />
+    </div>
+  </a>
+</li>
+
+<li className="carouselTicker__item">
+  <a href="https://yoast.com" target="_blank" rel="noopener noreferrer">
+    <div className="item-logo">
+      <img src="/assets/imgs/page/homepage3/yoastseo.svg" alt="Yoast SEO" />
+    </div>
+  </a>
+</li>
                 </Marquee>
         </>
     )
