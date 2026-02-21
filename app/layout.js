de3ca0500094b43dc545c78e74b20ede64c004a6
infodigisunny <infodigisunny@gmail.com>
2026-02-21 14:10:59 +0530

    s

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,61 @@
 /** @format */
 
-import { Urbanist } from "next/font/google";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "/public/assets/css/style.css";
+import { Urbanist } from "next/font/google"
+import Script from "next/script"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "/public/assets/css/style.css"
+
 const urban = Urbanist({
   weight: ["200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--tg-body-font-family",
   display: "swap",
-});
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={urban.variable}>{children}</body>
-    </html>
-  );
-}
+    <html lang="en">
+      <body className={urban.variable}>
 
+        {/* Organization Schema */}
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "@id": "https://adfinityclarity.com/#organization",
+              "name": "Adfinity Clarity",
+              "url": "https://adfinityclarity.com/",
+              "logo": {
+                "@type": "ImageObject",
+                "inLanguage": "en-US",
+                "@id": "https://adfinityclarity.com/#/schema/logo/image/",
+                "url": "https://adfinityclarity.com/full-logo.webp",
+                "contentUrl": "https://adfinityclarity.com/full-logo.webp",
+                "width": 289,
+                "height": 76,
+                "caption": "Adfinity Clarity"
+              },
+              "image": {
+                "@id": "https://adfinityclarity.com/#/schema/logo/image/"
+              },
+              "sameAs": [
+                "https://www.facebook.com/adfinityclarity/",
+                "https://www.youtube.com/@AdfinityClarity",
+                "https://www.instagram.com/adfinityclarity/",
+                "https://www.linkedin.com/company/adfinity-clarity/"
+              ]
+            })
+          }}
+        />
+
+        {children}
+      </body>
+    </html>
+  )
+}
