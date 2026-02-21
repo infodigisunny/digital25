1e26018983011d560dd78f780ae8b808e8227a6f
infodigisunny <infodigisunny@gmail.com>
2026-02-21 16:30:40 +0530

    s

--- components/sections/home3/Section5.js
+++ components/sections/home3/Section5.js
@@ -14,7 +14,7 @@ export default function Section5() {
                         <div className="row">
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question">
-                                    <div className="text-center"> <div className="btn btn-tag-black"><h5>GAIN CLARITY</h5></div></div><br></br>
+                                    <div className="text-center"> <div className="btn btn-tag-black"><h3>GAIN CLARITY</h3></div></div><br></br>
                                    <div className="card-info">
                                         <p className="text-md neutral-600">Before we touch your site, we dig into your market. That means understanding your goals, how your customers search, what your competitors are doing right (and wrong), and where search opportunities exist. No assumptions, just a clear view of what we&#39;re working with.</p>
                                     </div>
@@ -22,7 +22,7 @@ export default function Section5() {
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question bg-1">
-                                    <div className="text-center"> <div className="btn btn-tag-black"><h5>REFINE</h5></div></div><br></br>
+                                    <div className="text-center"> <div className="btn btn-tag-black"><h3>REFINE</h3></div></div><br></br>
                                     <div className="card-info">
                                         <p className="text-md neutral-600">We fix the issues that are impacting the technical underpinnings of your website. This includes speed, crawlability, indexing issues, mobile experience — pretty much anything that impacts your site. If it slows down rankings or user experience, we fix it; it&#39;s that simple.</p>
                                     </div>
@@ -30,7 +30,7 @@ export default function Section5() {
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question bg-2">
-                                    <div className="text-center"> <div className="btn btn-tag-black"><h5>OPTIMIZE</h5></div></div><br></br>
+                                    <div className="text-center"> <div className="btn btn-tag-black"><h3>OPTIMIZE</h3></div></div><br></br>
                                     <div className="card-info">
                                         <p className="text-md neutral-600">Now we work hard to optimize your content assets. From on-page copy, meta tags, internal links, and content structure, we are aligning everything to match real search intent. This is not just about keywords; it also aims to deliver great clarity, value and relevance for your audience.</p>
                                     </div>
@@ -38,7 +38,7 @@ export default function Section5() {
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question bg-3">
-                                    <div className="text-center"> <div className="btn btn-tag-black"><h5>WIN AUTHORITY</h5></div></div><br></br>
+                                    <div className="text-center"> <div className="btn btn-tag-black"><h3>WIN AUTHORITY</h3></div></div><br></br>
                                     <div className="card-info">
                                         <p className="text-md neutral-600">Finally, we earn your trust that you are looking for. This is achieved by building high-quality backlinks, strengthening your off-site presence and growing your domain authority. In a way, both search engines and real-life people will trust: we use no shortcuts, no spam, just credible mentions, and creative outreach.</p>
                                     </div>
@@ -46,7 +46,7 @@ export default function Section5() {
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question bg-4">
-                                   <div className="text-center"> <div className="btn btn-tag-black"><h5>TRACK</h5></div></div><br></br>
+                                   <div className="text-center"> <div className="btn btn-tag-black"><h3>TRACK</h3></div></div><br></br>
                                    <div className="card-info">
                                         <p className="text-md neutral-600">There is also no wait and see if it works, we track everything in real-time. We track your rankings, traffic, and all behaviour on your site through GA4, GSC, and custom dashboards.<br></br> If something changes, we adapt early and keep the momentum going.</p>
                                     </div>
@@ -54,7 +54,7 @@ export default function Section5() {
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <div className="card-question bg-5">
-                                    <div className="text-center"> <div className="btn btn-tag-black"><h5>HANDOFF INSIGHTS</h5></div></div><br></br>
+                                    <div className="text-center"> <div className="btn btn-tag-black"><h3>HANDOFF INSIGHTS</h3></div></div><br></br>
                                     <div className="card-info">
                                         <p className="text-md neutral-600">You are never in the dark. We will provide consistent reports that actually make sense, and there will never be any black-box or data dump SEO. Your team gains value, context, and actionable recommendations they can act on, whether you are actively working on the project or handing it all off to us.
 </p>
