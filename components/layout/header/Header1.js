27a01be5f602984627a806215a93d53aeab390bd
infodigisunny <infodigisunny@gmail.com>
2026-02-23 18:37:01 +0530

    s

--- components/layout/header/Header1.js
+++ components/layout/header/Header1.js
@@ -265,11 +265,9 @@ export default function Header1({
     e.preventDefault();
     window.dispatchEvent(new CustomEvent("openProposalModal"));
   }}
+  style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
 >
   Get SEO Proposal
-  <svg width={22} height={22} viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <path d='M22 11.0003L18.4791 7.47949V10.3074H0V11.6933H18.4791V14.5213L22 11.0003Z' fill='true'></path>
-  </svg>
 </Link>
               <div
                 className='burger-icon burger-icon-white'
