4a5ece928025ed12641ffc4e906815763d0ccc3d
infodigisunny <infodigisunny@gmail.com>
2026-03-16 14:20:02 +0530

    ss

--- components/sections/home3/Section1.js
+++ components/sections/home3/Section1.js
@@ -53,7 +53,7 @@ export default function Section1() {
 
     return (
         <>
-            <section className="section-box hero-banner-section">
+            <section className="section-box" style={{ marginTop: "95px" }}>
   <div className="banner-hero hero-3">
     <div className="banner-inner">
       <div
