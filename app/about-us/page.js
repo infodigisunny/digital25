80bbff954cd05a39c8f3b71c601bd59180583fc3
infodigisunny <infodigisunny@gmail.com>
2026-04-29 21:47:26 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -8,6 +8,30 @@ export default function AboutUs() {
 
             <Layout headerStyle={1} footerStyle={1} headerCls="header-style-1 header-style-1" >
                <div style={{ marginTop: "100px" }}>
+                <section
+                          className='section-box box-content-blog'
+                          style={{ marginTop: "100px" }}>
+                          <div className='container'>
+                            <div className='text-center blog-head'>
+                              <span className='icon-1 shape-1' />
+                              <span className='icon-2 shape-2' />
+                              <span className='icon-3 shape-3' />
+                              <span className='btn btn-brand-4-sm'>Our Inside</span>
+                              <h2 className='heading-2 mb-20 mt-15'>Blog Resources</h2>
+                              <p className='text-lg'>
+                                Explore our blog and resources for valuable insights, expert
+                                opinions, and up-to-
+                                <br className='d-none d-lg-block' />
+                                date information on the latest trends in the industry.
+                              </p>
+                            </div>
+                            <div className='box-list-news mt-100'>
+                              <div className='row'>
+                                <BlogPost showItem={6} style={1} showPagination />
+                              </div>
+                            </div>
+                          </div>
+                        </section>
                 <section className="section-box box-content-term">
                     <div className="container">
                         <div className="text-center contact-head"><span className="btn btn-bg-brand-4 mb-15"><span>
@@ -125,7 +149,7 @@ export default function AboutUs() {
                         </div>
                     </div>
                 </section>
-</div>
+              </div>
             </Layout >
         </>
     )
