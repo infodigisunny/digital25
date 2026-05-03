95b243b266366faa30bbc57508ed05f4d0add53f
infodigisunny <infodigisunny@gmail.com>
2026-05-03 13:21:45 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -132,7 +132,6 @@ We are not here to sell you something and then disappear. We are here to help yo
              <div className="row">
                                 <div className="col-lg-4 col-md-6">
                                     <div className="card-question card-question-2">
-                                        <div className="card-image"><img src="/assets/imgs/page/features/internet.png" alt="Adfinity Clarity" /></div>
                                         <div className="card-info">
                                             <h3>How we think?</h3>
                                             <p className="text-md neutral-600">Clarity is not our name. It is how we work. We do not use language or make reports that are hard to understand. When something is working, we tell you why. When it is not working, we tell you that too. We bring a solution. Honest conversations help everyone.</p>
@@ -141,7 +140,6 @@ We are not here to sell you something and then disappear. We are here to help yo
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="card-question card-question-2">
-                                        <div className="card-image"><img src="/assets/imgs/page/features/train.png" alt="Adfinity Clarity" /></div>
                                         <div className="card-info">
                                             <h3>What do we try to do?</h3>
                                             <p className="text-md neutral-600">We want to make high-quality search and digital marketing available to all businesses, not just big ones with big budgets. We want to help the business in Bangalore reach buyers in Brisbane, or the agency in Manchester compete with companies. Where you are should not determine if you can grow online. A good strategy should.</p>
@@ -150,7 +148,6 @@ We are not here to sell you something and then disappear. We are here to help yo
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="card-question card-question-2">
-                                        <div className="card-image"><img src="/assets/imgs/page/features/pricing.png" alt="Adfinity Clarity" /></div>
                                         <div className="card-info">
                                             <h3>What makes us different?</h3>
                                             <p className="text-md neutral-600">We do not change the people who work on your account every month. The people you meet at the start are the ones who do the work. They know your industry, they track your competitors. They care about your numbers.</p>
