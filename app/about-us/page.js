54e39f6582eb52c4be9893983dc485e234f0ea3f
infodigisunny <infodigisunny@gmail.com>
2026-05-03 15:30:08 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -377,6 +377,24 @@ We are not here to sell you something and then disappear. We are here to help yo
               </div>
             </div>
           </section>
+          <div className="container mt-25">
+                            <div className="box-newsletter">
+                                <div className="newsletter-left">
+                                    <h2>Join our community</h2>
+                                    <p className="text-md neutral-600">Work smarter with this time management and not hard join our newsletter for a better expertrience</p>
+                                </div>
+                                <div className="newsletter-right">
+                                    <form action="#">
+                                        <input className="form-control" type="text" placeholder="Email address..." />
+                                        <button className="btn btn-subscribe" type="submit">Subscribe
+                                            <svg width={22} height={22} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M22 10.9993L18.4791 7.47852V10.3064H0V11.6924H18.4791V14.5203L22 10.9993Z" fill="true" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
         </div>
       </Layout>
     </>
