d0fc92073222d204328421f43c23879b71669329
infodigisunny <infodigisunny@gmail.com>
2026-05-03 15:42:53 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -380,10 +380,19 @@ We are not here to sell you something and then disappear. We are here to help yo
                             <div className="box-newsletter">
                                 <div className="newsletter-centre">
                                     <h2>Your competitors are getting found. You should be, too!</h2>
-                                    <p className="text-md neutral-600">There is no secret to search. It is consistent, well-executed work by people who know what they are doing. We do that work every day for businesses in four countries across industries.</p>
-                                    <p className="text-md neutral-600">If your website is not bringing in the leads, it should let us find out why.</p>
-                                </div>
+                                    <p className="text-md neutral-600">There is no secret to search. It is consistent, well-executed work by people who know what they are doing. We do that work every day for businesses in four countries across industries. If your website is not bringing in the leads, it should let us find out why.</p>
+                                    </div>
                             </div>
+                            <div className="newsletter-centre">
+                                    <form action="#">
+                                        <input className="form-control" type="text" placeholder="Email address..." />
+                                        <button className="btn btn-subscribe" type="submit">Subscribe
+                                            <svg width={22} height={22} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M22 10.9993L18.4791 7.47852V10.3064H0V11.6924H18.4791V14.5203L22 10.9993Z" fill="true" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
                         </div>
         </div>
       </Layout>
