f9d2f0e3b1dad020e28892fb494c67940bbd69b4
infodigisunny <infodigisunny@gmail.com>
2026-05-03 15:55:33 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -384,8 +384,7 @@ We are not here to sell you something and then disappear. We are here to help yo
                                 </div><br></br>
                                 <div className="newsletter-centre">
                                     <form action="#">
-                                        <input className="form-control" type="text" placeholder="Email address..." />
-                                        <button className="btn btn-subscribe" type="submit">Subscribe
+                                        <button className="btn btn-subscribe" type="submit">Get a free search audit, no strings attached
                                             <svg width={22} height={22} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M22 10.9993L18.4791 7.47852V10.3064H0V11.6924H18.4791V14.5203L22 10.9993Z" fill="true" />
                                             </svg>
