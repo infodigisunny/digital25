61d4153fe0b6f09eb905ee0444e72ee4f87cf184
infodigisunny <infodigisunny@gmail.com>
2026-05-04 14:38:18 +0530

    s

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -6,6 +6,7 @@ import Layout from "@/components/layout/Layout";
 import OfficeSlider from "@/components/slider/OfficeSlider";
 import Team2Slider from "@/components/slider/Team2Slider";
 import Link from "next/link";
+import Section1 from "@/components/sections/popup/Section1";
 
 export const metadata = {
   title: "About Award-Winning SEO Company | Adfinity Clarity",
@@ -51,6 +52,7 @@ export default function About() {
     <>
       <ResizeContainer />
       <Layout headerStyle={1} footerStyle={1} logoWhite>
+        
         <div style={{ marginTop: "100px" }}>
 
           {/* ── Section 1: Hero ── */}
@@ -425,6 +427,7 @@ We are not here to sell you something and then disappear. We are here to help yo
   </div>
 </div>
         </div>
+        <Section1 />
       </Layout>
       
     </>
