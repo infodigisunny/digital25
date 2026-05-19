070960087ae03e3957ba979f03d9a84d66b91ad7
infodigisunny <infodigisunny@gmail.com>
2026-05-19 10:17:18 +0530

    s

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Urbanist } from "next/font/google"
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
-import "/public/assets/css/style.css"
+
 
 const urban = Urbanist({
   weight: ["200", "300", "400", "500", "600", "700"],
@@ -185,6 +185,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="stylesheet" href="/assets/css/style.css" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
