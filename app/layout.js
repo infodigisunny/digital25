c36bcc2c93f05df1a376f6c51de46adfdbeca6b3
infodigisunny <infodigisunny@gmail.com>
2026-05-19 09:34:51 +0530

    s

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Urbanist } from "next/font/google";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "/assets/css/style.css";
+
 const urban = Urbanist({
   weight: ["200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -15,8 +15,10 @@ const urban = Urbanist({
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel="stylesheet" href="/assets/css/style.css" />
+      </head>
       <body className={urban.variable}>{children}</body>
     </html>
   );
 }
-
